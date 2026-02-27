a9cf87c611e434a7b9cf64b36c31d2f16935c789
Ranjith Poojari <kranjith0799@gmail.com>
2026-02-27 12:52:51 +0530

    Fixed port for Render

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
